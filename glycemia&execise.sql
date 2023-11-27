508361d506ffa69c789eef1f9cc3f91797d2973d
Victor Hu <victorhu493@gmail.com>
2023-11-27 14:52:07 +0800

    've built the schema scoped to the exercise&healthcare
    New Features:
    1. alter the schema name to 'glycemia'
    2. limit the length of datatime&timestamp to 6
    3. drop the redundant schema prefix
    4. add the numerical suffix to differentiate the constraint names

--- glycemia&execise.sql
+++ glycemia&execise.sql
@@ -10,20 +10,20 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema glucose_monitoring
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `glucose_monitoring` ;
+DROP SCHEMA IF EXISTS `glycemia` ;
 
 -- -----------------------------------------------------
 -- Schema glucose_monitoring
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `glucose_monitoring` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
-USE `glucose_monitoring` ;
+CREATE SCHEMA IF NOT EXISTS `glycemia` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
+USE `glycemia` ;
 
 -- -----------------------------------------------------
--- Table `glucose_monitoring`.`user`
+-- Table `user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `glucose_monitoring`.`user` ;
+DROP TABLE IF EXISTS `user` ;
 
-CREATE TABLE IF NOT EXISTS `glucose_monitoring`.`user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `user_id` INT NOT NULL,
   `age` INT NULL,
   `name` VARCHAR(45) NULL,
@@ -36,29 +36,29 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `glucose_monitoring`.`doctor`
+-- Table `doctor`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `glucose_monitoring`.`doctor` ;
+DROP TABLE IF EXISTS `doctor` ;
 
-CREATE TABLE IF NOT EXISTS `glucose_monitoring`.`doctor` (
+CREATE TABLE IF NOT EXISTS `doctor` (
   `doctor_id` INT NOT NULL,
   `department` VARCHAR(45) NULL,
   `title` VARCHAR(45) NULL,
   PRIMARY KEY (`doctor_id`),
-  CONSTRAINT `doctor_ud`
+  CONSTRAINT `doctor_id`
     FOREIGN KEY (`doctor_id`)
-    REFERENCES `glucose_monitoring`.`user` (`user_id`)
+    REFERENCES `user` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `glucose_monitoring`.`profile`
+-- Table `profile`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `glucose_monitoring`.`profile` ;
+DROP TABLE IF EXISTS `profile` ;
 
-CREATE TABLE IF NOT EXISTS `glucose_monitoring`.`profile` (
+CREATE TABLE IF NOT EXISTS `profile` (
   `patient_id` INT NOT NULL,
   `gender` ENUM('Female', 'Male') NULL DEFAULT 'Male',
   `address` GEOMETRY NULL,
@@ -75,173 +75,173 @@ CREATE TABLE IF NOT EXISTS `glucose_monitoring`.`profile` (
   `oral_therapy` TINYINT NULL,
   `insulin_therapy` TINYINT NULL,
   PRIMARY KEY (`patient_id`),
-  CONSTRAINT `patient_id`
+  CONSTRAINT `patient_id0`
     FOREIGN KEY (`patient_id`)
-    REFERENCES `glucose_monitoring`.`user` (`user_id`)
+    REFERENCES `user` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `glucose_monitoring`.`exercise`
+-- Table `exercise`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `glucose_monitoring`.`exercise` ;
+DROP TABLE IF EXISTS `exercise` ;
 
-CREATE TABLE IF NOT EXISTS `glucose_monitoring`.`exercise` (
+CREATE TABLE IF NOT EXISTS `exercise` (
   `patient_id` INT NULL,
-  `start_time` DATETIME(14) NULL,
+  `start_time` DATETIME(6) NULL,
   `duration` INT NULL COMMENT 'unit:min',
   `category` VARCHAR(45) NULL COMMENT '\'jogging,yoga,swimming,running\'',
   `exercise_id` INT NOT NULL,
   PRIMARY KEY (`exercise_id`),
-  CONSTRAINT `patient_id`
+  CONSTRAINT `patient_id1`
     FOREIGN KEY (`patient_id`)
-    REFERENCES `glucose_monitoring`.`profile` (`patient_id`)
+    REFERENCES `profile` (`patient_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `glucose_monitoring`.`glycemia`
+-- Table `glycemia`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `glucose_monitoring`.`glycemia` ;
+DROP TABLE IF EXISTS `glycemia` ;
 
-CREATE TABLE IF NOT EXISTS `glucose_monitoring`.`glycemia` (
+CREATE TABLE IF NOT EXISTS `glycemia` (
   `patient_id` INT NOT NULL,
   `glycemia` INT NULL,
-  `record_time` DATETIME(14) NOT NULL,
+  `record_time` DATETIME(6) NOT NULL,
   PRIMARY KEY (`record_time`, `patient_id`),
   CONSTRAINT `patient_id`
     FOREIGN KEY (`patient_id`)
-    REFERENCES `glucose_monitoring`.`profile` (`patient_id`)
+    REFERENCES `profile` (`patient_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `glucose_monitoring`.`insulin`
+-- Table `insulin`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `glucose_monitoring`.`insulin` ;
+DROP TABLE IF EXISTS `insulin` ;
 
-CREATE TABLE IF NOT EXISTS `glucose_monitoring`.`insulin` (
+CREATE TABLE IF NOT EXISTS `insulin` (
   `patient_id` INT NOT NULL,
   `dose` DECIMAL NULL,
-  `time` TIMESTAMP(14) NOT NULL,
+  `time` TIMESTAMP(6) NOT NULL,
   PRIMARY KEY (`patient_id`, `time`),
-  CONSTRAINT `patient_id`
+  CONSTRAINT `patient_id2`
     FOREIGN KEY (`patient_id`)
-    REFERENCES `glucose_monitoring`.`profile` (`patient_id`)
+    REFERENCES `profile` (`patient_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `glucose_monitoring`.`treatment`
+-- Table `treatment`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `glucose_monitoring`.`treatment` ;
+DROP TABLE IF EXISTS `treatment` ;
 
-CREATE TABLE IF NOT EXISTS `glucose_monitoring`.`treatment` (
+CREATE TABLE IF NOT EXISTS `treatment` (
   `doctor_id` INT NOT NULL,
   `patient_id` INT NOT NULL,
-  `treat_time` TIMESTAMP(14) NOT NULL,
+  `treat_time` TIMESTAMP(6) NOT NULL,
   `prescription` TEXT(200) NULL,
   `effect` VARCHAR(45) NULL,
   PRIMARY KEY (`doctor_id`, `patient_id`, `treat_time`),
   INDEX `patient_id_idx` (`patient_id` ASC) VISIBLE,
-  CONSTRAINT `patient_id`
+  CONSTRAINT `patient_id3`
     FOREIGN KEY (`patient_id`)
-    REFERENCES `glucose_monitoring`.`profile` (`patient_id`)
+    REFERENCES `profile` (`patient_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `doctor_id`
+  CONSTRAINT `doctor_id2`
     FOREIGN KEY (`doctor_id`)
-    REFERENCES `glucose_monitoring`.`doctor` (`doctor_id`)
+    REFERENCES `doctor` (`doctor_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `glucose_monitoring`.`working_hours`
+-- Table `working_hours`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `glucose_monitoring`.`working_hours` ;
+DROP TABLE IF EXISTS `working_hours` ;
 
-CREATE TABLE IF NOT EXISTS `glucose_monitoring`.`working_hours` (
+CREATE TABLE IF NOT EXISTS `working_hours` (
   `doctor_id` INT NOT NULL,
   `duration_mark` INT NOT NULL,
   `start_time` TIME NULL,
   `start_day` VARCHAR(15) NULL COMMENT '\'From Monday to Friday\'',
   `end_time` TIME NULL COMMENT 'unit:hour',
   PRIMARY KEY (`doctor_id`, `duration_mark`),
-  CONSTRAINT `doctor_id`
+  CONSTRAINT `doctor_id3`
     FOREIGN KEY (`doctor_id`)
-    REFERENCES `glucose_monitoring`.`doctor` (`doctor_id`)
+    REFERENCES `doctor` (`doctor_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `glucose_monitoring`.`examine`
+-- Table `examine`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `glucose_monitoring`.`examine` ;
+DROP TABLE IF EXISTS `examine` ;
 
-CREATE TABLE IF NOT EXISTS `glucose_monitoring`.`examine` (
+CREATE TABLE IF NOT EXISTS `examine` (
   `examination_id` INT NOT NULL,
   `patient_id` INT NULL,
   `weight` DECIMAL NULL,
   `health_state` ENUM('severe', 'unhealthy', 'well', 'healthy') NULL,
   `high_blood_pressure` INT NULL COMMENT 'mmHg',
   `sleep_quality` ENUM('terrible', 'well', 'good') NULL,
-  `examine_time` DATETIME(14) NULL,
+  `examine_time` DATETIME(6) NULL,
   `low_blood_pressure` INT NULL COMMENT 'unit:mmHg',
   `hyperglycemia` ENUM('limosis', 'after meal', 'euglycemia') NULL,
   `trend` ENUM('fluctuate', 'steady', 'often hyperglycemia', 'often hypoglycemia') NULL,
   `exercise_dose` ENUM('Seldom(sleep/sedentary/in hospital)', 'Medium', 'High', 'Low') NULL,
   PRIMARY KEY (`examination_id`),
   INDEX `patient_id_idx` (`patient_id` ASC) VISIBLE,
-  CONSTRAINT `patient_id`
+  CONSTRAINT `patient_id4`
     FOREIGN KEY (`patient_id`)
-    REFERENCES `glucose_monitoring`.`profile` (`patient_id`)
+    REFERENCES `profile` (`patient_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `glucose_monitoring`.`composition`
+-- Table `composition`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `glucose_monitoring`.`composition` ;
+DROP TABLE IF EXISTS `composition` ;
 
-CREATE TABLE IF NOT EXISTS `glucose_monitoring`.`composition` (
+CREATE TABLE IF NOT EXISTS `composition` (
   `patient_id` INT NOT NULL,
   `symptom` ENUM('diabetic foot', 'diabetic eye', 'diabetic kidney', 'diabetic cardiovascular disease', ' diabetic neuropathy', 'diabetic skin disease', 'hypertension', 'hyperlipidemia', 'others') NOT NULL,
   PRIMARY KEY (`patient_id`, `symptom`),
-  CONSTRAINT `patient_id`
+  CONSTRAINT `patient_id5`
     FOREIGN KEY (`patient_id`)
-    REFERENCES `glucose_monitoring`.`profile` (`patient_id`)
+    REFERENCES `profile` (`patient_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `glucose_monitoring`.`heart_rate`
+-- Table `heart_rate`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `glucose_monitoring`.`heart_rate` ;
+DROP TABLE IF EXISTS `heart_rate` ;
 
-CREATE TABLE IF NOT EXISTS `glucose_monitoring`.`heart_rate` (
+CREATE TABLE IF NOT EXISTS `heart_rate` (
   `exercise_id` INT NOT NULL,
   `interval_seq` INT NOT NULL COMMENT 'per 3 minutes',
   `avg_interval_heart_rate` INT NULL,
   PRIMARY KEY (`exercise_id`, `interval_seq`),
   CONSTRAINT `exercise_id`
     FOREIGN KEY (`exercise_id`)
-    REFERENCES `glucose_monitoring`.`exercise` (`exercise_id`)
+    REFERENCES `exercise` (`exercise_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
