5dbf970969c864b32c10bf38cb1db1ec44c378a7
Victor Hu <victorhu493@gmail.com>
2023-12-15 22:37:33 +0800

    wegs schema才有权限

--- glycemia.sql
+++ glycemia.sql
@@ -10,12 +10,12 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema glucose_monitoring
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `glycemia` ;
+DROP SCHEMA IF EXISTS `wegs` ;
 
 -- -----------------------------------------------------
 -- Schema glucose_monitoring
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `glycemia` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
+CREATE SCHEMA IF NOT EXISTS `wegs` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
 USE `glycemia` ;
 
 -- -----------------------------------------------------
