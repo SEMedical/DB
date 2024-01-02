5ee15b845842f6e5574a5f635a8adc27ae36712b
Victor Hu <victorhu493@gmail.com>
2024-01-02 11:29:48 +0800

    fix the unit of glycemia statistics to mmol/L
    SEMedical/.github#10

--- glycemia.sql
+++ glycemia.sql
@@ -231,21 +231,21 @@ SELECT
   HOUR(`record_time`) BETWEEN 7 AND 9 OR
   HOUR(`record_time`) BETWEEN 12 AND 14 OR
   HOUR(`record_time`) BETWEEN 17 AND 19)
-  AND `glycemia` > 140 THEN 1 WHEN NOT (
+  AND `glycemia` > 7.770 THEN 1 WHEN NOT (
   HOUR(`record_time`) BETWEEN 7 AND 9 OR
   HOUR(`record_time`) BETWEEN 12 AND 14 OR
   HOUR(`record_time`) BETWEEN 17 AND 19)
-  AND `glycemia` > 100 THEN 1 ELSE 0 END) / COUNT(`glycemia`)) * 100 AS `hyper_percent`,
-  (SUM(CASE WHEN `glycemia` < 70 THEN 1 ELSE 0 END) / COUNT(`glycemia`)) * 100 AS `hypo_percent`,
+  AND `glycemia` > 5.550 THEN 1 ELSE 0 END) / COUNT(`glycemia`)) * 5.550 AS `hyper_percent`,
+  (SUM(CASE WHEN `glycemia` <5.550 THEN 1 ELSE 0 END) / COUNT(`glycemia`)) * 5.550 AS `hypo_percent`,
   1 - (SUM(CASE WHEN (
   HOUR(`record_time`) BETWEEN 7 AND 9 OR
   HOUR(`record_time`) BETWEEN 12 AND 14 OR
   HOUR(`record_time`) BETWEEN 17 AND 19)
-  AND `glycemia` > 140 THEN 1 WHEN NOT (
+  AND `glycemia` > 7.770 THEN 1 WHEN NOT (
   HOUR(`record_time`) BETWEEN 7 AND 9 OR
   HOUR(`record_time`) BETWEEN 12 AND 14 OR
   HOUR(`record_time`) BETWEEN 17 AND 19)
-  AND `glycemia` > 100 THEN 1 ELSE 0 END) / COUNT(`glycemia`)) * 100 AS `eu_percent`
+  AND `glycemia` > 5.550 THEN 1 ELSE 0 END) / COUNT(`glycemia`)) * 5.550 AS `eu_percent`
 FROM
   `glycemia`
 GROUP BY
